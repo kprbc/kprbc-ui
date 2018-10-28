f71559be7532e52adcf545d04fb4f0ccdfb3c726
Emmanuel Goh <emmanuel@visenze.com>
2018-10-28 15:32:12 +0800

    Create layout with v-app, drawer and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon />
+      <v-toolbar-title>KPRBC</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App"
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
